88948a600a0e5ff62896126ab94db790b40de551
luis jose sanchez carrasco <luissancar@yahoo.es>
2024-11-01 18:05:17 +0100

    dos4

--- app/src/main/java/com/example/tercerapartecomponentes2025/MainActivity.kt
+++ app/src/main/java/com/example/tercerapartecomponentes2025/MainActivity.kt
@@ -7,9 +7,14 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.tercerapartecomponentes2025.ui.theme.TerceraparteComponentes2025Theme
@@ -33,14 +38,27 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+    var show by rememberSaveable { mutableStateOf(false) }
+    var enaButton by rememberSaveable { mutableStateOf(true) }
     Column(modifier = modifier.fillMaxSize()) {
-       // MyCard()
+        // MyCard()
         //MyBadgeBox()
-       // MyDivider()
+        // MyDivider()
         //MyDropDownMenu()
         //MySlider()
         //MySlider02()
-        MyAlertDialog01()
+        //MyAlertDialog01()
+        Button(onClick = {
+            show = true
+            enaButton = false
+        }
+        ) {
+            Text(text = "Show")
+        }
+        MyCustonDialog(show) {
+            show = false
+            enaButton = true
+        }
     }
 }
 
---
+++ app/src/main/java/com/example/tercerapartecomponentes2025/MyCustonDialog.kt
@@ -0,0 +1,112 @@
+package com.example.tercerapartecomponentes2025
+
+
+import androidx.annotation.DrawableRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.AlertDialogDefaults
+import androidx.compose.material3.BasicAlertDialog
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.DialogProperties
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyCustonDialog(show: Boolean, onDismiss: () -> Unit) {
+
+
+    if (show)
+        BasicAlertDialog(
+            onDismissRequest = {
+                // Descarta el diálogo cuando el usuario hace clic fuera del diálogo o en el botón de retroceso.
+                // Si deseas desactivar esa funcionalidad, simplemente utiliza un onDismissRequest vacío
+                onDismiss()
+            },
+        ) {
+            Surface(  //Surface es un contenedor que proporciona un fondo con un color
+                // y características de material design, como elevación y forma.
+                modifier = Modifier
+                    .wrapContentWidth()
+                    .wrapContentHeight(),
+                shape = MaterialTheme.shapes.large,
+                tonalElevation = AlertDialogDefaults.TonalElevation
+            ) {
+                Column(modifier = Modifier.padding(16.dp)) {
+                    MyTittle("Título")
+                    ItemCuenta("mail@gmail.com",R.drawable.ima01)
+                    ItemCuenta("mail@gmail.com",R.drawable.ima02)
+                    ItemCuenta("Add",R.drawable.ic_launcher_background)
+                }
+            }
+        }
+}
+
+
+@Composable
+fun ItemCuenta(email: String, @DrawableRes drawable: Int) { // es una referencia de un drawable
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Image(
+            painter = painterResource(drawable),
+            contentScale = ContentScale.Crop,
+            contentDescription = "Descripción",
+            modifier = Modifier
+                .padding(8.dp)
+                .size(40.dp)
+                .clip(CircleShape)
+        )
+
+        Text(
+            email,
+            fontSize = 14.sp,
+            modifier = Modifier.padding(8.dp)
+        )
+
+
+
+    }
+}
+
+
+@Composable
+fun MyTittle(texto: String) {
+    Text(
+        texto,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 25.sp,
+        modifier = Modifier.padding(bottom = 14.dp)
+    )
+}
